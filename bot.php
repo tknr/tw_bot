f92277329d3eac5174df12ef0bd17c1c935591c1
tknr <geishagirlskoume@gmail.com>
2015-04-07 17:41:37 +0900

    test

--- bot.php
+++ bot.php
@@ -1,18 +1,21 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('timezone', 'Asia/Tokyo');
-error_reporting(E_ALL);
-setlocale(LC_ALL, 'ja_JP.UTF-8');
+{
+    ini_set('display_errors', 1);
+    ini_set('timezone', 'Asia/Tokyo');
+    error_reporting(E_ALL);
+    setlocale(LC_ALL, 'ja_JP.UTF-8');
+    require_once (__DIR__ . '/vendor/autoload.php');
+    require_once (__DIR__ . '/lib/ReflexiveLoader.php');
+    Logger::configure(__DIR__ . '/config/log4php.xml');
+    $loader = new ReflexiveLoader();
+    $loader->registerDir(__DIR__ . '/lib');
+}
 
 $config = parse_ini_file(__DIR__ . '/config/config.ini', true);
+foreach ($config['file'] as $key => $file) {
+    $config['file'][$key] = __DIR__ . $file;
+}
 print_r($config);
-
-require_once (__DIR__ . '/config/config.php');
-require_once (__DIR__ . '/vendor/autoload.php');
-require_once (__DIR__ . '/lib/TweetTextReader.php');
-require_once (__DIR__ . '/lib/TwitterBot.php');
-
-Logger::configure(__DIR__ . '/config/log4php.xml');
 
 $bot = new TwitterBot(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET);
 if (! $bot->isVerifyed()) {
